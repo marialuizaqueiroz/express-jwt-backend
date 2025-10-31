222130932681b186a180db71dee5d816950552dc
marialuizaqueiroz <marialuqrls2005@gmail.com>
2025-10-31 14:04:34 -0300

    tentativa correção vercel

--- src/app.ts
+++ src/app.ts
@@ -22,29 +22,30 @@ app.use(errorMiddleware);
 // --- CORREÇÃO DA LÓGICA ASSÍNCRONA ---
 
 // 1. Criamos uma função async 'startServer' para "embrulhar" o arranque
-const startServer = async () => {
-  try {
-    logger.info("🔍 Tentando conectar ao MongoDB...");
+// const startServer = async () => {
+//   try {
+//     logger.info("🔍 Tentando conectar ao MongoDB...");
     
-    // 2. Usamos 'await' para garantir que a conexão termine
-    await connectDB({
-      serverSelectionTimeoutMS: 30000, 
-      socketTimeoutMS: 45000, 
-    });
+//     // 2. Usamos 'await' para garantir que a conexão termine
+//     await connectDB({
+//       serverSelectionTimeoutMS: 30000, 
+//       socketTimeoutMS: 45000, 
+//     });
     
-    // 3. Este log agora SÓ roda DEPOIS do 'await' ser bem-sucedido
-    logger.info("✅ Conexão com MongoDB estabelecida!");
+//     // 3. Este log agora SÓ roda DEPOIS do 'await' ser bem-sucedido
+//     logger.info("✅ Conexão com MongoDB estabelecida!");
 
-    // 4. O servidor SÓ começa a ouvir DEPOIS que o banco está pronto
-    app.listen(config.port, () =>
-      logger.info(`🚀 Servidor rodando na porta ${config.port}`)
-    );
-  } catch (error: any) {
-    // 5. O 'catch' agora apanha erros tanto do connectDB() como do app.listen()
-    logger.error("❌ Falha ao iniciar o servidor:", error.message || error);
-    process.exit(1); 
-  }
-};
+//     // 4. O servidor SÓ começa a ouvir DEPOIS que o banco está pronto
+//     app.listen(config.port, () =>
+//       logger.info(`🚀 Servidor rodando na porta ${config.port}`)
+//     );
+//   } catch (error: any) {
+//     // 5. O 'catch' agora apanha erros tanto do connectDB() como do app.listen()
+//     logger.error("❌ Falha ao iniciar o servidor:", error.message || error);
+//     process.exit(1); 
+//   }
+// };
 
 // 6. Chamamos a função para arrancar tudo
-startServer();
+// startServer();
+export default app;
